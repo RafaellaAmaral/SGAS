bbb96a2719f15696ef30e215d1886297725da363
RAFAELLA FERNANDES CORTEZ DO AMARAL <127240010+RafaellaAmaral@users.noreply.github.com>
2025-12-12 19:33:51 -0300

    corrigindo

--- SGAS/src/main/java/proj/security/SecurityConfig.java
+++ SGAS/src/main/java/proj/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -48,9 +47,9 @@ public class SecurityConfig {
         return http.build();
     }
 
-	@Bean
+		@Bean
 	PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-	}
+		return new sha512HexPasswordEncoder();
 
+}
 }
